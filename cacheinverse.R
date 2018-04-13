7768b9eb286b0dc0c7a04441d1ea9f333ca0b4a2
vinipav <38329744+vinipav@users.noreply.github.com>
2018-04-13 13:36:52 -0400

    Update cacheinverse.R

--- cacheinverse.R
+++ cacheinverse.R
@@ -4,15 +4,20 @@
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 
-makeCacheMatix <- function(x = matrix()) {  
+makeCacheMatix <- function(x = matrix()) { 
+  #Initiate an empty entry for the invers variable (inverse matrix), when a new cache 
+        #matrix object is generated
   invers <- NULL  
   set <- function(y) {  
     x <<- y   
     invers <<- NULL 
 } 
+  #return the value of the x variable as passed when making the 'CacheMatrix' object
+        #or when re-set using the $set function of the object
   get <- function() x 
   setInverse <- function(mean) invers <<- inverse 
   getInverse <- function() invers  
+  #Index the list with '$' callable descriptors
   list(set = set, get = get,     
         setInverse = setInverse, 
         getInverse = getInverse)
@@ -26,13 +31,17 @@ makeCacheMatix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...)
 { 
+  ## Return a matrix that is the inverse of 'x'
   invers <- x$getInverse() 
+   #if the invers variable is not NA, then print some text and return the matrix stored there
   if(!is.null(invers)) {  
     message("getting cached data")    
   return(invers) 
 }
   data <- x$get() 
   invers <- mean(data, ...) 
+  #Call setInverse function from makeCacheMatrix and reassign variable stored for that
+        #object
   x$setmmakeCacheMatix <- function(x = matrix()) { 
   invers <- NULL  set <- function(y) {  
   x <<- y  
